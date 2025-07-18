1ea11a2bb176ad3d07460ef90723dc0e2cce2c56
Hemant5hk <hemanthrajendran@gmail.com>
2025-07-18 20:00:46 +0530

    Update main.tf

--- .github/workflows/main.tf
+++ .github/workflows/main.tf
@@ -3,6 +3,14 @@ provider "azurerm" {
 }
 
 resource "azurerm_resource_group" "example" {
-  name     = "example-resource-group"
+  name     = "example-resources"
   location = "East US"
+}
+
+resource "azurerm_storage_account" "example" {
+  name                     = "examplestoracc123"
+  resource_group_name      = azurerm_resource_group.example.name
+  location                 = azurerm_resource_group.example.location
+  account_tier             = "Standard"
+  account_replication_type = "LRS"
 }
