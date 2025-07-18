886df8a07385d1d656e971b07eff19b990431353
Hemant5hk <hemanthrajendran@gmail.com>
2025-07-18 19:38:38 +0530

    Update main.tf

--- .github/workflows/main.tf
+++ .github/workflows/main.tf
@@ -6,3 +6,10 @@ resource "azurerm_resource_group" "example" {
   name     = "example-resource-group"
   location = "East US"
 }
+
+resource "azuread_user" "userhk" {
+  user_principal_name = "jdoe@hashicorp.com"
+  display_name        = "J. Doe"
+  mail_nickname       = "jdoe"
+  password            = "SecretP@sswd99!"
+}
